2c468b7535ade2f678957eb3a66e07e67cf51d36
adrhmnhkm <adrhmnhkm@gmail.com>
2025-09-11 19:04:52 +0700

    chore: Add auto-refresh deployment URL to CORS

--- src/backend/server.js
+++ src/backend/server.js
@@ -17,6 +17,7 @@ app.use(cors({
     'https://frontend-only-310k3qwix-adrhmnhkms-projects.vercel.app',
     'https://frontend-only-1h49n2l66-adrhmnhkms-projects.vercel.app',
     'https://frontend-only-bhlbog584-adrhmnhkms-projects.vercel.app',
+    'https://frontend-only-qlliozfar-adrhmnhkms-projects.vercel.app',
     'http://localhost:3000',
     'http://localhost:5173',
     /\.vercel\.app$/
