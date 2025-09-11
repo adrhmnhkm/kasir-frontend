f3a6261433c50ee2ab2bd281a25179b7c72fe8b3
adrhmnhkm <adrhmnhkm@gmail.com>
2025-09-11 22:29:13 +0700

    chore: Add ReportsPage debug deployment URL to CORS

--- src/backend/server.js
+++ src/backend/server.js
@@ -19,6 +19,7 @@ app.use(cors({
     'https://frontend-only-bhlbog584-adrhmnhkms-projects.vercel.app',
     'https://frontend-only-qlliozfar-adrhmnhkms-projects.vercel.app',
     'https://frontend-only-na4nrhq01-adrhmnhkms-projects.vercel.app',
+    'https://frontend-only-hu1xiiemk-adrhmnhkms-projects.vercel.app',
     'http://localhost:3000',
     'http://localhost:5173',
     /\.vercel\.app$/
