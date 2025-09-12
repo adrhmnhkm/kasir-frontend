2f910f3008fd4c146d91164267ae5b6c455a5863
adrhmnhkm <adrhmnhkm@gmail.com>
2025-09-12 09:06:11 +0700

    chore: Add local Tailwind deployment URL to CORS

--- src/backend/server.js
+++ src/backend/server.js
@@ -31,6 +31,7 @@ app.use(cors({
     'https://frontend-only-36mnvxcls-adrhmnhkms-projects.vercel.app',
     'https://frontend-only-bky7a3gtk-adrhmnhkms-projects.vercel.app',
     'https://frontend-only-dj4scyzr2-adrhmnhkms-projects.vercel.app',
+    'https://frontend-only-exo06l3so-adrhmnhkms-projects.vercel.app',
     'http://localhost:3000',
     'http://localhost:5173',
     /\.vercel\.app$/
